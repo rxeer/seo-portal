627a4278886c4906244ca21e9bf2a624f5cfac0c
FreAK19 <andrii.kiri4encko@yandex.ru>
2019-05-16 23:56:05 +0300

    update package.json

--- src/components/QdtComponent.jsx
+++ src/components/QdtComponent.jsx
@@ -8,7 +8,7 @@ const options = {//
     host: 'bipro.prozorro.org', // 'sense-demo.qlik.com', orange.rbcgrp.com, bipro.prozorro.org'
     secure: false, //
     port: 80, // 443
-    prefix: 'new', // www
+    prefix: 'new2', // www
     appId: '1def32a1-4fbb-4df7-b7a6-956de8dc6c3c', // '133dab5d-8f56-4d40-b3e0-a6b401391bde', // Helpdesk
   },
   connections: {
